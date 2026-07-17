8b4edb82fadeb53b3d9ad26993470d2c126c748c
Yagami <tranet513@gmail.com>
2026-07-17 16:37:55 +0700

    add AuthController to handle authentication-related routes

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { ForgotPasswordDto } from './dto/forgot-password.dto';
+import { ResetPasswordDto } from './dto/reset-password-dto.dto';
+
+@Controller('auth')
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService
+    ) {}
+
+    @Post('register')
+    register(@Body() dto:RegisterDto){
+        return this.authService.register(dto);
+    }
+
+    @Post('verify-otp')
+    verifyOtp(@Body() dto:{userId:string, otp:string}){
+        return this.authService.verifyOTP(dto)
+    }
+
+    @Post('login')
+    login(@Body() dto: LoginDto) {
+        return this.authService.login(dto);
+    }
+
+    @Post('forgot-password')
+    forgotPassword(@Body() dto: ForgotPasswordDto) {
+        return this.authService.forgotPassword(dto);
+    }
+
+    @Post('reset-password')
+    resetPassword(@Body() dto: ResetPasswordDto) {
+        return this.authService.resetPassword(dto);
+    }
+}

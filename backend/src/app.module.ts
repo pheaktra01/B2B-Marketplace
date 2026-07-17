2ead1e759f531b9093a76c15f9c72784da455afb
Yagami <tranet513@gmail.com>
2026-07-17 16:38:32 +0700

    add TypeOrmModule configuration and import AuthModule and UsersModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host:process.env.DB_HOST,
+      port:Number(process.env.DB_PORT),
+      username:process.env.DB_USERNAME,
+      password:process.env.DB_PASSWORD,
+      database:process.env.DB_DATABASE,
+
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

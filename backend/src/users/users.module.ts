ab5ba8066fea5f11d9db79d76d730a781c976683
Yagami <tranet513@gmail.com>
2026-09-03 09:41:21 +0700

    Add Order entity to TypeOrmModule in UsersModule

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from '../order/entities/order.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User, Order]),
   ],
   exports: [
     TypeOrmModule,

23e135d21b36cac2cad121170f876f1411d030e8
Yagami <tranet513@gmail.com>
2026-09-11 09:13:02 +0700

    Add NotificationModule to ProductsModule for product notifications

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -5,10 +5,12 @@ import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { Product } from './enterties/product.entity';
 import { User } from '../users/entities/user.entity';
+import { NotificationModule } from '../notification/notification.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Product, User]),
+    NotificationModule,
   ],
 
   controllers: [
